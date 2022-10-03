b636ac332b1cb189dd21c57a2fa61e8f66219a68
Eliaquim Santos <emauricio@tasken.com>
2022-10-03 10:58:37 -0300

    Testando deploy automatico.

--- PUC.MinhaAcademia.API/EndpointsMapper.cs
+++ PUC.MinhaAcademia.API/EndpointsMapper.cs
@@ -8,7 +8,7 @@ namespace PUC.MinhaAcademiaPlus.API
         {
             app.MapPost("/login", (MainService service, Login login) => service.Logar(login).CreateToken()).AllowAnonymous();
             app.MapGet("/aluno", (MainService service) => service.ConsultarDadosAluno()).RequireAuthorization();
-
+            app.MapGet("/teste", (MainService service) => service.ConsultarDadosAluno()).RequireAuthorization();
             return app;
         }
     }
